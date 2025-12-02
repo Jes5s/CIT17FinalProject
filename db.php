f135268cf20f3d284f48b467dabf2b4b345866c3
Jes5s <JsslDssn@gmail.com>
2025-12-03 00:02:50 +0800

    Create db

---
+++ db.php
@@ -0,0 +1,10 @@
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "wellness_db";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
